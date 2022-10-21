e15e210e4132e982a4dd46246de7721e34e6a69b
Luca Strano <stranoluca469955@gmail.com>
2022-10-21 20:43:57 +0200

    fix: remove some comments

--- main.py
+++ main.py
@@ -10,14 +10,11 @@ def reply(update, context):
     text = update.message.text.lower()
 
     translator = Translator()
-    #translate_text = 
     update.message.reply_text(translator.translate(text, src='it', dest='en').text)
-
-    #update.message.reply_text(translate_text)
    
-updater = Updater(TOKEN) #La classe updater ci permette di controllare il nostro bot, quindi gli passiamo il TOKEN relativo
-updater.dispatcher.add_handler(CommandHandler("start", start)) #Quando il bot riceve il comando start allora la funzione "start" deve gestirlo
-updater.dispatcher.add_handler(MessageHandler(Filters.text, reply)) #Stesso concetto per i messaggi mandati da noi sul bot
+updater = Updater(TOKEN) 
+updater.dispatcher.add_handler(CommandHandler("start", start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, reply))
 
 print("bot in ascolto ...")
-updater.start_polling() #Per mantenere sempre il bot in ascolto
+updater.start_polling()
